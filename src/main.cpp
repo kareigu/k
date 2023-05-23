4951ec60964b28213873094ba2184c9c81acb520
karei <eetu.kivelae@gmail.com>
2023-05-23 23:10:47 +0300

    Init

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <raylib-cpp.hpp>
+#include <fmt/core.h>
+#include <string_view>
+#include "log.h"
+#include "raylib.h"
+#include <argumentum/argparse-h.h>
+
+constexpr int screen_width = 800;
+constexpr int screen_height = 600;
+constexpr int target_fps = 60;
+
+void UpdateDrawFrame(raylib::Window&);
+
+int main() {
+    log::info("Creating window with {:d}x{:d}@{:d}", screen_width, screen_height, target_fps);
+    raylib::Window window(screen_width, screen_height, "Raygame");
+
+    SetTargetFPS(target_fps);
+    while (!window.ShouldClose())
+    {
+        UpdateDrawFrame(window);
+    }
+
+    return 0;
+}
+
+void UpdateDrawFrame(raylib::Window& window) {
+    window.BeginDrawing();
+        ClearBackground(BLACK);
+        raylib::Text(fmt::format("{:d} fps | {:f} ms", window.GetFPS(), window.GetFrameTime()), 30, RAYWHITE, ::GetFontDefault(), 2.0f).Draw(190, 200);
+    window.EndDrawing();
+}
