20b8ca0b4f3272c0fead1574ff0cf753f539e76f
karei <eetu.kivelae@gmail.com>
2023-05-23 23:30:02 +0300

    Allow changing resolution via cli arguments

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,34 @@
 #include <raylib-cpp.hpp>
 #include <string_view>
 
-constexpr int screen_width = 800;
-constexpr int screen_height = 600;
-constexpr int target_fps = 60;
+constexpr int DEFAULT_SCREEN_WIDTH = 800;
+constexpr int DEFAULT_SCREEN_HEIGHT = 600;
+constexpr int DEFAULT_TARGET_FPS = 60;
 
 void UpdateDrawFrame(raylib::Window&);
 
-int main() {
+int main(int argc, char** argv) {
+  auto args = argumentum::argument_parser();
+  auto params = args.params();
+
+  int screen_width;
+  params.add_parameter(screen_width, "-w", "--width")
+          .required(false)
+          .default_value(DEFAULT_SCREEN_WIDTH)
+          .maxargs(1);
+  int screen_height;
+  params.add_parameter(screen_height, "-h", "--height")
+          .required(false)
+          .default_value(DEFAULT_SCREEN_HEIGHT)
+          .maxargs(1);
+  int target_fps;
+  params.add_parameter(target_fps, "-f", "--target-fps")
+          .required(false)
+          .default_value(DEFAULT_TARGET_FPS)
+          .maxargs(1);
+  args.add_default_help_option();
+  args.parse_args(argc, argv);
+
   log::info("Creating window with {:d}x{:d}@{:d}", screen_width, screen_height, target_fps);
   raylib::Window window(screen_width, screen_height, "Raygame");
 
