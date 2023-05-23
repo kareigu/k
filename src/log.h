e81dbbc007c8725b2e07c19e423596ff0d3bf8df
karei <eetu.kivelae@gmail.com>
2023-05-23 23:13:13 +0300

    clang-format

--- src/log.h
+++ src/log.h
@@ -1,26 +1,26 @@
 #pragma once
 #include "raylib.h"
-#include <raylib-cpp.hpp>
 #include <fmt/core.h>
+#include <raylib-cpp.hpp>
 
 namespace log {
-    template<typename... T>
-    void info(fmt::format_string<T...> string, T&&... args) {
-        TraceLog(LOG_INFO, fmt::format(string, args...).c_str());
-    }
-
-    template<typename... T>
-    void error(fmt::format_string<T...> string, T&&... args) {
-        TraceLog(LOG_ERROR, fmt::format(string, args...).c_str());
-    }
-
-    template<typename... T>
-    void warn(fmt::format_string<T...> string, T&&... args) {
-        TraceLog(LOG_WARNING, fmt::format(string, args...).c_str());
-    }
-
-    template<typename... T>
-    void debug(fmt::format_string<T...> string, T&&... args) {
-        TraceLog(LOG_DEBUG, fmt::format(string, args...).c_str());
-    }
+template<typename... T>
+void info(fmt::format_string<T...> string, T&&... args) {
+  TraceLog(LOG_INFO, fmt::format(string, args...).c_str());
 }
+
+template<typename... T>
+void error(fmt::format_string<T...> string, T&&... args) {
+  TraceLog(LOG_ERROR, fmt::format(string, args...).c_str());
+}
+
+template<typename... T>
+void warn(fmt::format_string<T...> string, T&&... args) {
+  TraceLog(LOG_WARNING, fmt::format(string, args...).c_str());
+}
+
+template<typename... T>
+void debug(fmt::format_string<T...> string, T&&... args) {
+  TraceLog(LOG_DEBUG, fmt::format(string, args...).c_str());
+}
+}// namespace log
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-#include <raylib-cpp.hpp>
-#include <fmt/core.h>
-#include <string_view>
 #include "log.h"
 #include "raylib.h"
 #include <argumentum/argparse-h.h>
+#include <fmt/core.h>
+#include <raylib-cpp.hpp>
+#include <string_view>
 
 constexpr int screen_width = 800;
 constexpr int screen_height = 600;
@@ -12,21 +12,20 @@ constexpr int target_fps = 60;
 void UpdateDrawFrame(raylib::Window&);
 
 int main() {
-    log::info("Creating window with {:d}x{:d}@{:d}", screen_width, screen_height, target_fps);
-    raylib::Window window(screen_width, screen_height, "Raygame");
+  log::info("Creating window with {:d}x{:d}@{:d}", screen_width, screen_height, target_fps);
+  raylib::Window window(screen_width, screen_height, "Raygame");
 
-    SetTargetFPS(target_fps);
-    while (!window.ShouldClose())
-    {
-        UpdateDrawFrame(window);
-    }
+  SetTargetFPS(target_fps);
+  while (!window.ShouldClose()) {
+    UpdateDrawFrame(window);
+  }
 
-    return 0;
+  return 0;
 }
 
 void UpdateDrawFrame(raylib::Window& window) {
-    window.BeginDrawing();
-        ClearBackground(BLACK);
-        raylib::Text(fmt::format("{:d} fps | {:f} ms", window.GetFPS(), window.GetFrameTime()), 30, RAYWHITE, ::GetFontDefault(), 2.0f).Draw(190, 200);
-    window.EndDrawing();
+  window.BeginDrawing();
+  ClearBackground(BLACK);
+  raylib::Text(fmt::format("{:d} fps | {:f} ms", window.GetFPS(), window.GetFrameTime()), 30, RAYWHITE, ::GetFontDefault(), 2.0f).Draw(190, 200);
+  window.EndDrawing();
 }
